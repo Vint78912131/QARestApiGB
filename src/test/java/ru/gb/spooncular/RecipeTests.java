367e1451c23b5e514474062917453f8c8602d69f
Vitaliy <starozhil2008@mail.ru>
2022-04-18 22:47:27 +0300

    180422_2

--- src/test/java/ru/gb/spooncular/RecipeTests.java
+++ src/test/java/ru/gb/spooncular/RecipeTests.java
@@ -1,7 +1,17 @@
 package ru.gb.spooncular;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import io.restassured.path.json.JsonPath;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.given;
 import static junit.framework.Assert.*;
@@ -15,30 +25,54 @@ public class RecipeTests {
     public int tr_Garlic = 1765;
     public int id_644218 = 644218;
     public String component_Plum = "Plum";
-    public String url = "https://spoonacular.com/recipes/";
+    public String url = "https://spoonacular.com/";
+    OkHttpClient okHttpClient = new OkHttpClient()
+            .newBuilder()
+            .readTimeout(10, TimeUnit.SECONDS)
+            .build();
 
 
 
     @Test
-    public void getRecipePositiveTest() {
-        given()
-                .queryParam("apiKey", apiKey)
-                .when()
-                .get(url + "complexSearch")
-                .then()
-                .statusCode(200);
+    public void getRecipePositiveTest() throws IOException {
+//        Request request = new Request.Builder()
+//        .addHeader("apiKey", apiKey)
+//        .url("https://spoonacular.com/recipes/complexSearch")
+//        .get()
+//        .build();
+//
+//        Response response = okHttpClient.newCall(request).execute();
+//        System.out.println(response.body().string());
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        String results = objectMapper.readTree(body).asText();
+
+          String response = given()
+                  //.queryParam("apiKey", apiKey)
+//                  .queryParam("Host", "<calculated when request is sent>")
+//                  .queryParam("User-Agent", "PostmanRuntime/7.29.0")
+//                  .queryParam("Accept", "*/*")
+//                  .queryParam("Accept-Encoding", "gzip, deflate, br")
+//                  .queryParam("apiKeyConnection", "keep-alive")
+                  .when()
+                  .get("https://spoonacular.com/recipes/complexSearch?apiKey=a1eb2ff3a89c40289fe2c3d6af7cd53a")
+                .asPrettyString();
+
+
+        System.out.println(response);
     }
 
     @Test
-    public void getRecipePositiveAssertTest() {
-        given()
+    public void postCuisinePositiveAssertTest() {
+        String response = given()
                 .queryParam("apiKey", apiKey)
+                //.queryParam("includeNutrition","false")
                 .when()
-                .get(url + "complexSearch")
+                .post(url + "cuisine")
                 .then()
-                .assertThat()
-                .statusCode(200)
-                .and().
+                .extract()
+                .body().asPrettyString();
+        System.out.println(response);
+
 //                .body("offset",is(0))
 //                .body("number",is(10))
 //                .body("totalResults",equalTo(5222))
