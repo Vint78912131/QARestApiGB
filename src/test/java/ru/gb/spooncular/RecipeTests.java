09de721bb26bff31d4cb5ba430916e01983bc560
sotnikov@a-3.ru <sotnikov@a-3.ru>
2022-04-21 16:29:02 +0300

    commit210422_1

--- src/test/java/ru/gb/spooncular/RecipeTests.java
+++ src/test/java/ru/gb/spooncular/RecipeTests.java
@@ -2,23 +2,33 @@ package ru.gb.spooncular;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.github.javafaker.Faker;
 import io.qameta.allure.Description;
 import io.qameta.allure.Step;
+import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import lombok.SneakyThrows;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import ru.gb.retrofit.dto.ProductDto;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.given;
 import static junit.framework.Assert.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static ru.gb.retrofit.util.RetrofitUtil.getCategoryService;
+import static ru.gb.retrofit.util.RetrofitUtil.getProductsService;
 
 public class RecipeTests {
     public String apiKey = "a1eb2ff3a89c40289fe2c3d6af7cd53a";
@@ -28,13 +38,20 @@ public class RecipeTests {
     public int id_644218 = 644218;
     public String component_Plum = "Plum";
     public String url = "https://api.spoonacular.com/recipes/";
-
-
+    //@SneakyThrows
+//    @BeforeEach
+//    void setUp() {
+//        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+//    }
     @Test
     @Description("Checking offset, number, totalResults with assertThat")
     public void getRecipePositiveTest() throws IOException {
         JsonPath response = given()
+                .log()
+                .all()
                 .queryParam("apiKey", apiKey)
+                .expect()
+                .body("results[0].title", equalTo("Cauliflower, Brown Rice, and Vegetable Fried Rice"))
                 .when()
                 .get(url+"complexSearch")
                 .body()
@@ -46,15 +63,19 @@ public class RecipeTests {
     }
 
     @Test
-    @Description("Checking offset, number, totalResults with expect()")
+    @Description("Checking response with expect()")
     public void getRecipeWithBodyChecksGivenPositiveTest() {
         given()
+                .log()
+                .all()
                 .queryParam("apiKey", apiKey)
                 .queryParam("cuisine", "Greek")
                 .expect()
                 .body("offset", is(0))
                 .body("number", is(10))
                 .body("totalResults", is(24))
+                .body("results[0].title", equalTo("Classic Greek Moussaka"))
+                .body("results[1].id", is(664678))
                 .when()
                 .get(url+"complexSearch")
                 .then()
@@ -67,6 +88,8 @@ public class RecipeTests {
     @Description("Checking cuisine, confidence")
     public void postCuisinePositiveAssertTest() {
         JsonPath response = given()
+                .log()
+                .all()
                 .queryParam("apiKey", apiKey)
                 .when()
                 .post(url + "cuisine")
@@ -77,5 +100,10 @@ public class RecipeTests {
         assertThat(response.get("confidence"), equalTo((float)(0)));
     }
 
+//    @SneakyThrows
+//    @AfterEach
+//    void tearDown() {
+//        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+//    }
 
 }
